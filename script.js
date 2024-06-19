522ad2452fc44569281a71bb0ec6fedd0c2856ca
Lucas-PC <lgbs@cesar.school>
2024-06-19 16:58:04 -0300

    disable button on generating team

--- script.js
+++ script.js
@@ -1,46 +1,52 @@
 class TeamMaker {
   constructor(qnt_members = 6, repeat_types = false) {
-      this.qnt_members = qnt_members;
-      this.repeat_types = repeat_types;
-      this.team_members = [];
-      this.url = 'https://pokeapi.co/api/v2/pokemon';
+    this.qnt_members = qnt_members;
+    this.repeat_types = repeat_types;
+    this.team_members = [];
+    this.url = "https://pokeapi.co/api/v2/pokemon";
   }
 
   get_pokemon_type(pokemon) {
-      return pokemon.types.map(type => type.type.name)
+    return pokemon.types.map((type) => type.type.name);
   }
 
   get_pogemon_name(pokemon) {
-      return pokemon.name.replace(/-/g, ' ').replace(/\b\w/g, c => c.toUpperCase());;
+    return pokemon.name
+      .replace(/-/g, " ")
+      .replace(/\b\w/g, (c) => c.toUpperCase());
   }
 
   async get_random_pokemon() {
-      let pokemon_id = Math.floor(Math.random() * 1025);
-      let response = await fetch(`${this.url}/${pokemon_id}`);
-      let pokemon = await response.json();
-      return { game_indice: pokemon.id, name: this.get_pogemon_name(pokemon), type: this.get_pokemon_type(pokemon), sprite: pokemon.sprites.front_default };
+    let pokemon_id = Math.floor(Math.random() * 1025);
+    let response = await fetch(`${this.url}/${pokemon_id}`);
+    let pokemon = await response.json();
+    return {
+      game_indice: pokemon.id,
+      name: this.get_pogemon_name(pokemon),
+      type: this.get_pokemon_type(pokemon),
+      sprite: pokemon.sprites.front_default,
+    };
   }
 
   async randomize_team() {
-      let team = [];
-      let seenTypes = new Set();
-  
-      while (team.length < this.qnt_members) {
-          let pokemon = await this.get_random_pokemon();
-          let hasDuplicateType = pokemon.type.some(t => seenTypes.has(t));
-  
-          if (!this.repeat_types && hasDuplicateType) {
-              // Se não permitir tipos repetidos e o Pokémon tem um tipo que já está no time, continue para o próximo Pokémon
-              continue;
-          }
-  
-          team.push(pokemon);
-          pokemon.type.forEach(t => seenTypes.add(t)); // Adiciona os tipos deste Pokémon ao conjunto de tipos vistos
+    let team = [];
+    let seenTypes = new Set();
+
+    while (team.length < this.qnt_members) {
+      let pokemon = await this.get_random_pokemon();
+      let hasDuplicateType = pokemon.type.some((t) => seenTypes.has(t));
+
+      if (!this.repeat_types && hasDuplicateType) {
+        // Se não permitir tipos repetidos e o Pokémon tem um tipo que já está no time, continue para o próximo Pokémon
+        continue;
       }
-  
-      return team;
+
+      team.push(pokemon);
+      pokemon.type.forEach((t) => seenTypes.add(t)); // Adiciona os tipos deste Pokémon ao conjunto de tipos vistos
+    }
+
+    return team;
   }
-  
 }
 
 // Captura os elementos HTML
@@ -76,6 +82,9 @@ const typeColors = {
 
 // Função para gerar o time de Pokémon
 function generateTeam() {
+  generateTeamButton.classList.add('generating');
+  generateTeamButton.disabled = true;
+
   // Obtém o tamanho do time do input
   const teamSize = parseInt(teamSizeInput.value, 10);
 
@@ -109,14 +118,20 @@ function generateTeam() {
         <div class="pokemon-id">ID: ${pokemon.game_indice}</div>
         <div class="pokemon-sprite"><img src=${pokemon.sprite}></div>
         <div class="pokemon-name">${pokemon.name}</div>
-        <div class="pokemon-types">Tipos: ${pokemon.type.join(", ")}</div>
+        <div class="pokemon-types">Types: ${pokemon.type.join(", ")}</div>
     `;
         teamOutput.appendChild(pokemonElement);
       });
+
+      generateTeamButton.disabled = false;
+      generateTeamButton.classList.remove('generating');
     })
     .catch((error) => {
-      console.error("Erro ao gerar time:", error);
-      teamOutput.innerHTML = `<p>Ocorreu um erro ao gerar o time de Pokémon.</p>`;
+      console.error("Error to generate team:", error);
+      teamOutput.innerHTML = `<p>Error to generate team.</p>`;
+
+      generateTeamButton.disabled = false;
+      generateTeamButton.classList.remove('generating');
     });
 }
 
